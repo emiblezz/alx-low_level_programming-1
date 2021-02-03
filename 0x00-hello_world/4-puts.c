57354fed88545a20e891e438c1d4a6e797e0a4d9
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-02-03 18:29:11 +0000

    print a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - prints a text
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
