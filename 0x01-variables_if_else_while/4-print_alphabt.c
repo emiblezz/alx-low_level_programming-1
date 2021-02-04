fd9b42bbf49fe1422045700641fcdbd78fe97fd9
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-02-04 16:49:15 +0000

    print a to z exept q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alpha from a to z but don't include q and e
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
