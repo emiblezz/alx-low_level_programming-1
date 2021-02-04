d7bcca9262bc570dd1e9b047577aec13e192eae1
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-02-04 20:10:52 +0000

    two digit numbers different combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main - prints all possible different combinations of two digits.
+ * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
-			if (!((ones == tens) || (tens > ones)))
+			if (!((ones == tens) || (tens > ones)))/*eliminates repeatition*/
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))
+				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
