bf1a0cd8efc0789044df9896edccae3c5fab96f3
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-02-04 20:22:54 +0000

    three digit combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,21 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 	int hundreds = '0';
+
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 		{
 			for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 			{
-				if (!((ones == tens) || (hundreds == tens) || (tens > ones) || (hundreds > tens)))/*eliminates repeatition*/
+				if (!((ones == tens) || (hundreds == tens) ||
+				      (tens > ones) || (hundreds > tens)))/*eliminates repeatition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))/*addes comma and space*/
+					if (!(ones == '9' && hundreds == '7' &&
+					      tens == '8'))/*addes comma and space*/
 					{
 						putchar(',');
 						putchar(' ');
