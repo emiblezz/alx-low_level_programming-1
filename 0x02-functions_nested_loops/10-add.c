6e409e89956577819d571a999dc846cde08e6e3a
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-02-09 09:07:30 +0000

    adds two number

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - adds tow integers
- * @a - number 1 to be added
- * @b - number 2 to be added
+ * @a: number 1 to be added
+ * @b: number 2 to be added
  * Return: The result of the addition
  */
 int add(int a, int b)
