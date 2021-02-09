980210106aa2f19dfd83dd9e26b5868fd09fbc78
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-02-09 06:50:33 +0000

    print holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,8 @@
-#ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
 /**
  * _putchar -writes the character c to stdout
+ * @c: The input that will be printed
  * Return: 1 on succes and -1 on error
  */
 
