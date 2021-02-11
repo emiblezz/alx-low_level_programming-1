95a4052e783d7d11d00574d381cc7cedab5438d5
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-11 20:02:06 +0000

    corrected syntax

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,7 @@ void print_diagonal(int n)
 		{
 			i = column - 1;
 
-			for(space = 0; space <= i; space++)
+			for (space = 0; space <= i; space++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
