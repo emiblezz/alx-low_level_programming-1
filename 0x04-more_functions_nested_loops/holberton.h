6689f3309395337be79ffa4af908098653f5f8b5
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-11 20:50:05 +0000

    print triangle

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -74,4 +74,11 @@ void print_diagonal(int n);
 
 void print_square(int size);
 
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size:  is the size of the triangle
+ */
+
+void print_triangle(int size);
+
 #endif
