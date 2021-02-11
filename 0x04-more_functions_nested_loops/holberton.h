ba21fb46b05b4d8e49674a75966443a65e7c6d96
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-11 18:06:10 +0000

    Added a Readme and done question 0

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/**
+ * _isupper - cheches for uppercase character
+ * @c: The character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+
+#endif
