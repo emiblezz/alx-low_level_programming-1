0d34397b9e075c3320ae0b6c08e2255487e98f73
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-16 17:08:48 +0000

    resete to 98

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON
+#define _HOLBERTON
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @n: the parameter to be updated
+ */
+
+void reset_to_98(int *n);
+
+#endif
