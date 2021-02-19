7f3d4d6e3e88c1f4b242a3a2654ae0df0f5d35b0
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-02-19 20:38:33 +0300

    added another way of doing it

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -25,3 +25,18 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+/* Another way of doing it */
+/*
+	
+	   char *_strcat(char *dest, char *src)
+           {
+               int dest_len = strlen(dest);
+               int i;
+	       
+               for (i = 0 ; i < n && src[i] != '\0' ; i++)
+                   dest[dest_len + i] = src[i];
+               dest[dest_len + i] = '\0';
+
+               return dest;
+           }
+*/
