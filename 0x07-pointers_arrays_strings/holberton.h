8522428b1f15cd73cf3e48888386a80e3a27cfe7
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-22 17:29:12 +0000

    fils memory with constant byte

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _HOLBERTON
+#define _HOLBERTON
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _memset - fills a memory with a constant byte
+ * @s: pointer to memory area
+ * @b: constant value
+ * @n: number of bytest to fill in
+ * Return: filled memory byte
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
