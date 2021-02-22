e34f5471c60d1fb8f85d87e35093d43c9235f98e
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-22 18:24:18 +0000

    locates a substring

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -56,4 +56,12 @@ unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
 
+/**
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
+ */
+
+char *_strstr(char *haystack, char *needle);
 #endif
