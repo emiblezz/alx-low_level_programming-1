a121cc59135473e5244b2e998e37bd7385bcebed
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-22 18:17:56 +0000

    length

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -38,4 +38,13 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+
+unsigned int _strspn(char *s, char *accept);
+
 #endif
