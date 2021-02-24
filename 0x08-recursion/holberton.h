ad2aacd49976e41aed50cc328fef682f612619bd
Amanuel Sisay <amanabuelabiy.as@gmail.com>
2021-02-24 17:40:39 +0000

    first commit

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _HOLBERTON
+#define _HOLBERTON
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: The string
+ */
+
+void _puts_recursion(char *s);
+
+
+#endif
