4c0dff084ea8648f595cd14ac9f8de97d3b6eca5
Amanuel Sisay <amanauelabiy.as@gmail.com>
2021-03-02 11:27:50 +0000

    corrected 100-atoi.o

--- 0x09-static_libraries/100-atoi.c
+++
@@ -1,9 +0,0 @@
-#include "holberton.h"
-
-/**
- * _atoi - Takes a string and converts the numbers into integers
- * @s: The string to be checked
- * Return: Nothing, void
- */
-
-int _atoi(char *s);
