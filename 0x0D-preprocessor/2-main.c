bd66ff2f49b389677d08425656a48ab3c10f0b87
Amanuel Sisay <amanauelabiy.as@gmail.com>
2021-03-08 14:55:35 +0000

    compiled file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints the name of the compiled file
- * Return: Always 1 (on success)
+ * Return: Always 0 (on success)
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (1);
+	return (0);
 }
