acf1c2636ecc750356d0a60c59341d11a1d9477d
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-04 13:34:54 +0000

    added description

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ * Return: 0 Always (on Success)
+ */
+
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
