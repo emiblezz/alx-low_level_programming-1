063915c00984e36a6f850274b2679f1a355258f7
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-04 20:32:49 +0000

    last assembly code

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,14 @@
-global _start
+global    _start
 
-	section .text
+	          section   .text
+_start:	   mov       rax, 1	; system call for write
+	          mov       rdi, 1 ; file handle 1 is stdout
+	          mov       rsi, message ; address of string to output
+	          mov       rdx, 17	 ; number of bytes
+	          syscall		 ; invoke operating system to do the write
+	          mov       rax, 60	 ; system call for exit
+	          xor       rdi, rdi	 ; exit code 0
+	          syscall		 ; invoke operating system to exit
 
-_start:
-	  mov rax, 1        	; write(
-	  mov rdi, 1        	;   STDOUT_FILENO,
-	  mov rsi, msg      	;   "Hello, world!\n",
-	  mov rdx, msglen   	;   sizeof("Hello, world!\n")
-	  syscall           	; );
-
-	  mov rax, 60       	; exit(
-	  mov rdi, 0        	;   EXIT_SUCCESS
-	  syscall           	; );
-
-	section .rodata
-msg:	 db "Hello, Holberton", 10
-msglen:	 equ $ - msg
+	          section   .data
+message:	  db        "Hello, Holberton", 10 ; note the newline at the end
