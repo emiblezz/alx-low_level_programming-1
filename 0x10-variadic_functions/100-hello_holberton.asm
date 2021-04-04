71ee9772c941cbb0c2c65c3c4dc2957d202aff7a
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-04 20:29:11 +0000

    assembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,18 @@
-section .data
-
-	    message db "Hello, Holberton\n", 10
+global _start
 
 	section .text
 
-	global _start
 _start:
-	    mov rax, 1
-	    mov rdi, 1
-	    mov rsi, message
-	    mov rdx, 17
-	    syscall
+	  mov rax, 1        	; write(
+	  mov rdi, 1        	;   STDOUT_FILENO,
+	  mov rsi, msg      	;   "Hello, world!\n",
+	  mov rdx, msglen   	;   sizeof("Hello, world!\n")
+	  syscall           	; );
+
+	  mov rax, 60       	; exit(
+	  mov rdi, 0        	;   EXIT_SUCCESS
+	  syscall           	; );
+
+	section .rodata
+msg:	 db "Hello, Holberton", 10
+msglen:	 equ $ - msg
