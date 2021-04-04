45fb846330e6aa8abd94113e531ff819bbadc551
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-04 19:49:58 +0000

    corrected spelling

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
-	va_list arguments;
+	va_list valist;
 
-	va_start(arguments, n);
+	va_start(valist, n);
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg(argumentsm int);
-	va_end(arguments);
+		sum += va_arg(valist, int);
+	va_end(valist);
+
 	return (sum);
 }
