227e4057cae66baa05dfeb9b8aea37979f6ad642
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-08 08:57:11 +0000

    sum of all the data added description

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * summ_listint - adds all of the dat (n) in the linked list
+ * @head: pointer to head pointer of linked list
+ * Return: the sum or 0 if the list is empty
+ */
+
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
