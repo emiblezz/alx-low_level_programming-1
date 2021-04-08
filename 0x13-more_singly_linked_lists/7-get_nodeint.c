1b5740c483106d9bf94361ca456bcce4e7777484
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-08 08:41:47 +0000

    get that node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,11 +4,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	if (head == NULL)
-		return (NULL);
+	if (index == 0)
+		return (head);
 
-	while (index <= count)
+	while (count < index)
 	{
+		if (head->next == NULL)
+			return (NULL);
 		head = head->next;
 		count++;
 	}
