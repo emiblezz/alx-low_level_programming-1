48408ab345fa1bcadacc634c573b2c2f5a92edb9
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-08 08:44:31 +0000

    get that node discription added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * get_nodeint_at_index - get node at the given index
+ * @head: pointer to first node
+ * @index: node to locate
+ * Return: pointer to nth node
+ */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
