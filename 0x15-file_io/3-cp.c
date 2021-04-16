52418f4f1f7cc57cbfca2046fb6955bf78f0168c
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-04-16 13:24:41 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,18 +42,14 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		__exit(97, NULL, 0);
 
-	/*sets file descriptor for copy-to file*/
 	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_2 == -1)
 		__exit(99, argv[2], 0);
 
-	/*sets file descriptor for copy-from file*/
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 == -1)
 		__exit(98, argv[1], 0);
 
-	/*reads original file as long as there's more than 0 to read*/
-	/*copies/writes contents into new file */
 	while ((n_read = read(fd_1, buffer, 1024)) != 0)
 	{
 		if (n_read == -1)
