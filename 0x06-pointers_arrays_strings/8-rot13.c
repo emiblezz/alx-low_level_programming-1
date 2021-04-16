44a5d2ba7702b3b96d3da835eb6c1f7e8a34fa1a
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-04-16 10:36:24 +0000

    rot 13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,11 +1,24 @@
 #include "holberton.h"
-
 /**
- * rot13 - encode string using rot13
- * @s: string to encode
- * Return: encoded string
+ *rot13 - change value using
+ *@s: character
+ *Return: int
  */
-
 char *rot13(char *s)
 {
-
+	int i;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+				s[i] -= 13;
+			else
+				s[i] += 13;
+			i++;
+		}
+		i++;
+	}
+	return (s);
+}
