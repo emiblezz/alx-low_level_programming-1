389785b85bea7eb645ef827d0db03369633b6c94
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-04-16 13:16:47 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,21 +29,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	/* open file if it exists */
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	/* if nothing to write, still successful */
 	if (!text_content)
 	{
 		close(fd);
 		return (1);
 	}
 
-	/* write */
 	n_wrote = write(fd, text_content, _strlen(text_content));
-	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	if (n_wrote == -1)
 	{
 		close(fd);
 		return (-1);
