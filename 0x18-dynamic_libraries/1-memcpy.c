2153f81ad9fd91fca91700a08757c1ccf5088520
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-05-03 20:14:05 +0300

    added functions and their prototype

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copy n bytes of memory from source to destination
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < (int) n; i++)
+			dest[i] = src[i];
+	}
+
+	return (dest);
+}
