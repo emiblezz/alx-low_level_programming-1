fe479cf6cd6574afe56a6c25f2fe2f4e790c887e
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-05-03 20:44:35 +0000

    dynamic

--- 0x18-dynamic_libraries/9-strcpy.c
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -1,25 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
  * _strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest
