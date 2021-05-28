3c68f0278ff41ce4b4dcd011086c5eb9afd24717
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-05-28 16:37:58 +0300

    djbw hash function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,6 +15,7 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
+
 typedef struct hash_node_s
 {
      char *key;
@@ -38,5 +39,6 @@ typedef struct hash_table_s
 
 /* function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
