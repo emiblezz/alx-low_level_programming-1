b37f5e44dd4f9bdb1026611580f9bd38d4bf1b65
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-24 12:19:17 +0300

    sum of even fibonaccis less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints fibonacci numbers starting from 1
+ * Return: On successs (0)
+ */
+int main(void)
+{
+	long prev_num = 1;
+	long current_num = 2;
+	int sum = 0;
+
+	while (current_num < 4000000)
+	{
+		prev_num = current_num;
+		current_num += prev_num;
+		if (current_num % 2 == 0)
+			sum += current_num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
