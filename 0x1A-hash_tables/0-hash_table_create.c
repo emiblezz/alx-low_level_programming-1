cae7c69186848d466eaf7f3005d50658a7968230
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-07-24 15:41:27 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,29 +9,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_node_t **array;
-	unsigned long int i;
 
-	if (size <= 0)
+	if (!size)
+	{
 		return (NULL);
+	}
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
+	{
 		return (NULL);
+	}
 
-	array = malloc(sizeof(*array) * size);
-	if (!array)
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (!table->array)
 	{
 		free(table);
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-		array[i] = NULL;
-
 	table->size = size;
-	table->array = array;
-
 	return (table);
-
 }
