a425d8f7cb61d1c21af129c31ed8ed138dc65a12
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-01 13:43:37 +0300

    binary search iterative

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,40 +1,5 @@
 #include "search_algos.h"
 /**
- * binary_search_recursive - search through an array using binary search
- * @array: the array to be searched
- * @size: size of the array
- * @value: value to be searched
- * @left: left side start point of the array to be searched
- * @right: right side end poin of the array to be searched
- * Return: returns the index of the first value found else returns -1
- */
-int binary_search_recursive(int *array, size_t size,
-			     int value, int left, int right)
-{
-	if (left > right || right < left)
-		return (-1);
-	int mid = (left + right) / 2, i;
-
-	printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-	{
-		if (i == left)
-			printf("%d", array[i]);
-		else
-			printf(", %d", array[i]);
-	}
-	printf("\n");
-	if (array[mid] == value)
-		return (mid);
-	else if (array[mid] > value)
-		return (binary_search_recursive(array, size, value,
-						left, mid - 1));
-	else
-		return (binary_search_recursive(array, size, value,
-						mid + 1, right));
-}
-
-/**
  * binary_search - search through an array using binary search
  * @array: the array to be searched
  * @size: size of the array
@@ -43,7 +8,28 @@ int binary_search_recursive(int *array, size_t size,
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int res = binary_search_recursive(array, size, value, 0, (int)size - 1);
+	int left = 0;
+	int right = (int)size - 1;
 
-	return (res);
+	while (left < right)
+	{
+		int mid = (left + right) / 2, i;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+		if (i == left)
+			printf("%d", array[i]);
+			else
+			printf(", %d", array[i]);
+		}
+		printf("\n");
+		if (value == array[mid])
+			return (mid);
+		if (array[mid] > value)
+			right = mid - 1;
+		if (array[mid] < value)
+			left = mid + 1;
+	}
+	return (-1);
 }
