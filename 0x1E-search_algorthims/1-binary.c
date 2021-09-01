f0e57f7a85e3f2413a0b621409e8a06451e9365e
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-01 12:52:46 +0300

    binary search using iterative

--- 0x1E-search_algorthims/1-binary.c
+++ 0x1E-search_algorthims/1-binary.c
@@ -1,27 +1,42 @@
 #include "search_algos.h"
+
 /*
- * binary_serach - 
+ * binary_serach - search through an array using binary search
+ * @array: the array to be searched
+ * @size: size of the array
+ * @value: value to be searched
+ * Return: returns the index of the first value found else returns -1
  */
+
 int binary_search(int *array, size_t size, int value)
 {
 	int left = 0;
-	int right = int(size);
-	int mid = (left + right) / 2;
+	int right = (int)size - 1;
 
-	while (left <= right)
+
+	while (left < right)
 	{
+	        int mid = (left + right) / 2, i;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == left)
+				printf("%d", array[i]);
+			else
+				printf(", %d", array[i]);
+		}
+		printf("\n");
 		if (value == array[mid])
 			return mid;
 		if (array[mid] > value)
-		{
-			right = mid;
-			mid = (left + right) / 2;
-		}
+			right = mid - 1;
 		if (array[mid] < value)
-		{
-			left = mid;
-			mid = (left + right) / 2;
-		}
+			left = mid + 1;
 	}
 	return -1;
 }
+
+//int binary_search_recurssion(int *array, size_t size,
+//			     int value, int left, int right)
+//{
+//	if (value == 
