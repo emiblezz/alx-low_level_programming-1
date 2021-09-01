d938015f7b2daf3ef12da1d47f4ba5bac4f0c1a3
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-01 23:28:49 +0300

    added exceptions

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,10 +8,11 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (!array)
-		return (-1);
 	size_t left = 0;
 	size_t right = size - 1;
+
+	if (!array)
+                return (-1);
 
 	while (left <= right)
 	{
