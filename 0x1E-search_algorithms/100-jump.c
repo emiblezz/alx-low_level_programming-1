3103d8d9c5c9eedf77f46d38214782bee440d994
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-05 21:52:32 +0300

    fixed a syntax error

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -20,7 +20,7 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n",
 		       i, array[i]);
 		i += jump;
-		if (i > (int)size)
+		if (i >= (int)size)
 			break;
 	}
 	j = i - jump;
